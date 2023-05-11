898cdb36313c334279a59f95347b744418b13727
bingenk <s10244655@connect.np.edu.sg>
2023-05-11 10:42:53 +0800

    solve issues

--- app/src/main/java/sg/edu/np/mad/madpractical/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -9,10 +9,6 @@ public class User {
     public User() {
 
     }
-    public void setName() {
-        User user = new User("John", "A new user", 1, false);
-        user.user = "test";
-    }
 
     public User(String user,String description, int id,boolean followed){
         this.user=user;
