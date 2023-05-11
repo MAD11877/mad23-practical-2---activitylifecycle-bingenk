669f7b358480804ee93f5977aebe0b955054de72
bingenk <s10244655@connect.np.edu.sg>
2023-05-11 10:48:20 +0800

    solve issues

--- app/src/main/java/sg/edu/np/mad/madpractical/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -1,22 +1,51 @@
 package sg.edu.np.mad.madpractical;
 
 public class User {
-    public String user;
-    public String description;
-    public int id;
-    public boolean followed;
+    String name;
+    String description;
+    int id;
+    static boolean followed;
 
     public User() {
 
     }
 
     public User(String user,String description, int id,boolean followed){
-        this.user=user;
+        this.name=user;
         this.description=description;
         this.id=id;
         this.followed=followed;
 
     }
 
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getId() {
+        return id;
+    }
+    public static boolean isFollowed() {
+        return followed;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFollowed(boolean followed) {
+        this.followed = followed;
+    }
 
 }
